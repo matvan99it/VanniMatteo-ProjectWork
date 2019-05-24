ed03830c6f15871d5ea4e2edd3715a351f5df64b
MatVan99it <vannicraft@gmail.com>
2019-05-24 16:20:55 +0200

    test per l'iFrame non passato

--- src/pages/dettagli-contenuti/dettagli-contenuti.ts
+++ src/pages/dettagli-contenuti/dettagli-contenuti.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { WpApiProvider } from '../../providers/wp-api/wp-api';
+import { DomSanitizer } from '@angular/platform-browser';
 /**
  * Generated class for the DettagliContenutiPage page.
  *
@@ -20,16 +21,19 @@ export class DettagliContenutiPage {
   wp_img: any;
   public items:any=[];
   public dettagliato:any=[];
+  wp_text_allowed: any;
   
   public wp_ricomposto: any=["", "", "", ""];
 
-  constructor(public navCtrl: NavController, public api:WpApiProvider, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public api:WpApiProvider, public navParams: NavParams, private sanitizer: DomSanitizer) {
     this.wp_id=navParams.get('item');
     this.wp_type=navParams.get('tipologia');
     this.wp_text=navParams.get('contenuto');
     this.wp_img=navParams.get('foto');
+    
+    this.wp_text_allowed = sanitizer.bypassSecurityTrustUrl(this.wp_text);
     console.log("*************************");
-    console.log(this.wp_img+"   " +this.wp_text);
+    console.log(this.wp_img+" *** " +this.wp_text);
     
     this.api.get().subscribe((data)=>{
       console.log("banan" + data);
